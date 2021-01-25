2dea6d7c822928c0bb8e9f757ece6401526889b9
Mishra Gaurav <gm150180107025@gmail.com>
2021-01-25 20:56:20 +0530

    Add files via upload

---
+++ php/contactform.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/contactus.css">
+	<title>Contact Us</title>
+</head>
+<body>
+	<?php
+	require 'header.php';
+	?>
+	<style type="text/css">
+		body
+		{
+			background-color: #f6f5f5;
+		}
+		.ul-header-color{
+	        color: #333 !important;
+	    }
+	    .ul-header:first-of-type li:before{
+	        color:#3333336b;
+	    }
+	</style>
+<div class="container pt-5 mt-5">
+	<div class="row">
+		<div class="col-lg-12">
+			<h2 class="font-mont"><span style="color:#012b7d;">DON’T BE A STRANGER,</span><br/>
+            JUST SAY HELLO.</h2>
+		</div>
+	</div>
+	<div class="row contactpage mb-5">
+		<div class="col-lg-6">
+			<p class="font-nunito">Feel free to get in touch with us. We are always open to discussing new projects, creative ideas or opportunities & to be part of your visions.</p>
+			<img src="Svg/contact.svg" width="100%">
+		</div>
+		<div class="col-lg-6">
+			<div class="inputform">
+				<form>
+					<input type="text" placeholder="Full Name" class="input" name="fullname" required="required"><br/>
+	                <input type="text" placeholder="Phone number" class="input" name="phone" required="required"><br/>
+					<input type="email" placeholder="Email" class="input" name="email" required><br/>
+	                <select id="city" class="input" name="city">
+					  <option value="City">City</option>
+					  <option value="surat">Surat</option>
+					  <option value="Ahmedabad">Ahmedabad</option>
+					  <option value="Baroda">Baroda</option>
+					  <option value="Jamnagar">Jamnagar</option>
+					</select><br/>
+					<textarea placeholder="Write a message for any query....." class="input" name="message"></textarea>
+					<button class="button">Submit</button>
+				</form>
+		    </div>
+		</div>
+	</div>
+</div>
+<?php
+ require 'footer.php';
+?>
+<script type="text/javascript">
+	$('.ul-header-color').removeClass('header-a-tag-active');
+	$('.contact-a-tag').addClass('header-a-tag-active');
+</script>
+</body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,227 @@
+<style>
+    header{
+        position:fixed;
+    	display: -webkit-flex;
+    	display: -moz-flex;
+    	display: -ms-flex;
+    	display: -o-flex;
+    	display: flex;
+    	-webkit-flex-direction: row;
+    	-moz-flex-direction: row;
+    	-ms-flex-direction: row;
+    	-o-flex-direction: row;
+    	flex-direction: row;
+    	-ms-align-items: center;
+    	align-items: center;
+    	width:100vw;
+        top: 0;
+    	z-index: 10000;
+        padding: 10px 30px;
+    	transition:.3s;
+        background-color:transparent;
+    }
+    header.active{
+        box-shadow: 0 1rem 3rem rgba(0,0,0,.175)!important;
+        background-color:#ffffff;
+    }
+    header h2{
+        margin-left: 35px;
+    }
+    header nav{
+    	margin-left: auto;
+    	margin-right: 33px;
+    	transition: .5s;
+        display: flex !important;
+    	z-index: 300;
+    }
+    .menu-title{
+    	font-family: 'Montserrat',sans-serif;
+    	display: inline-block;
+    	text-transform: uppercase;
+    	font-size: 13px;
+    	font-weight: 600;
+    	padding: 19px;
+        color:#6d6d6d;
+        display:none;
+        margin-left: 34px;
+        margin-top:100px;
+    }
+    header ul{
+    	margin: 0;
+    	padding: 0;
+    	display: flex;
+    	-webkit-flex-direction: row;
+    	-moz-flex-direction: row;
+    	-ms-flex-direction: row;
+    	-o-flex-direction: row;
+    	flex-direction: row;
+    	-ms-align-items: center;
+    	align-items: center;
+    	counter-reset: mycount;
+    }
+    header ul:last-of-type{
+        display:none;
+    }
+    header ul li{
+    	font-family: 'Montserrat',sans-serif;
+    	display: inline-block;
+    	cursor: pointer;
+    	text-transform: uppercase;
+    	font-size: 13px;
+    	font-weight: 600;
+    	padding: 14px;
+    }
+    header ul li a{
+        color: #fff;
+        text-decoration:none;
+    }
+    header ul li a:hover{
+        color:#333!important;
+        text-decoration:none;
+    }
+    header ul:first-of-type li:before{
+    	counter-increment: mycount;
+    	content:'0'counter(mycount);
+    	margin-right: 6px;
+    	color:#ffffff6b;
+    }
+    header.active .ul-header-color{
+        color: #333;
+    }
+    header.active .ul-header:first-of-type li:before{
+        color:#3333336b;
+    }
+    .hamburger{
+    	position: absolute;
+    	left: 20px;
+    	top: 15px;
+    	width:30px;
+    	height:30px;
+    	display:none;
+    	align-items:center;
+    	z-index:600;
+    }
+    .hamburger-inner, .hamburger-inner:after, .hamburger-inner:before{
+    	background-color: #333;
+    	width: 30px;
+    	height:4px;
+    	display:block;
+    	border-radius:2px;
+    	transition: .3s;
+    }
+    .hamburger-inner:after{
+        content:' ';
+        transform:translateY(6px);
+    }
+    .hamburger-inner:before{
+        content:' ';
+        transform:translateY(-10px);
+    }
+    .hamburger.is-active > .hamburger-inner{
+        transform:rotate(45deg);
+    }
+    .hamburger.is-active > .hamburger-inner:after{
+        transform:translateY(-4px) rotate(90deg);
+    }
+    .hamburger.is-active > .hamburger-inner:before{
+        transform:translateY(0px) rotate(-90deg);
+    }
+    @media only screen and (max-width:930px){
+    	.hamburger{
+    		display: flex;
+    	}
+    	header img{
+            margin-left: 40px;
+        }
+    	header nav{
+    		background-color: #fff;
+    		position: fixed;
+    		display: block;
+    		width: 100vw;
+    		height: 100vh;
+    		top: 0;
+    		left: 0;
+    		margin: 0;
+    		transform: translateX(-100%);
+    	}
+        .menu-title{
+            padding-left: 0;
+            display:block;
+        }
+    	header nav ul:first-of-type{
+    		display: flex;
+    		-webkit-flex-direction: column;
+    		-moz-flex-direction: column;
+    		-ms-flex-direction: column;
+    		-o-flex-direction: column;
+    		flex-direction: column;
+    		align-items: unset;
+    		margin-top: 5px;
+    		margin-left: 20px;
+    	}
+    	header nav ul:last-of-type{
+    	    display: flex;
+    	    transform: translate(50%,0%) rotate(-90deg);
+    	    position: absolute;
+    	    right: 23px;
+    	}
+    	nav.showing{
+    		transform: translateX(0);
+    	}
+    }
+
+    .header-a-tag-active{
+        position: relative;
+    }
+    .header-a-tag-active:before{
+        content: '';
+        position: absolute;
+        height: 3px;
+        border-radius: 20px;
+        width: 100%;
+        background-color: #012b7d;
+        top: 100%;
+        margin-top: 3px;
+    }
+</style>
+<header>
+		<a href="/software_development/">
+            <img src="/software_development/images/Crux_logo.png" height="40px">      
+        </a>
+		<nav>
+		    <span class="menu-title">menu</span>
+			<ul class="ul-header">
+				<li><a class="ul-header-color header-a-tag-active" href="/software_development/">home</a></li>
+				<li><a class="ul-header-color about-a-tag" href="about">Our Works</a></li>
+				<li><a class="ul-header-color contact-a-tag" href="contact">Get a Quote</a></li>
+			</ul>
+			<ul>
+			    <li>facebook</li>
+			    <li>instagram</li>
+			    <li>twitter</li>
+			    <li>behance</li>
+			</ul>
+		</nav>
+		<div class="hamburger"><div class="hamburger-inner"></div></div>
+</header>
+<script>
+	$('.hamburger').click(function(){
+		if($(this).hasClass('is-active')){
+			$(this).removeClass('is-active');
+			$('nav').removeClass('showing');
+			$('header').css('z-index','100');
+		}else{
+			$(this).addClass('is-active');
+			$('nav').addClass('showing');
+			$('header').css('z-index','500');
+		}
+	});
+	$(window).scroll(function(){
+	    if($(this).scrollTop()==0){
+	        $('header').removeClass('active');
+	    }else{
+	        $('header').addClass('active');
+	    }
+	});
+	$('header nav ul:last-of-type').css('bottom',$('header nav ul:last-of-type').width()/2+50+'px');
+</script>
